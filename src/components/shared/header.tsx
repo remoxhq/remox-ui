65aef9c4acf4d11c318e91fc3cd6e652c700d6df
Nijat Hamid <nicatorium@gmail.com>
2024-02-14 16:55:45 +0400

    Layout Shifting solved

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -5,7 +5,7 @@ import WalletButtons from "@components/core/wallet-buttons";
 function Header() {
   return (
     <>
-      <header className="py-2 md:py-3 px-3 md:px-6 fixed top-0 left-0 right-0 h-14 md:h-16 lg:h-[4.5rem] w-full overflow-hidden z-50 bg-darkBlue border-b">
+      <header className="py-2 md:py-3 px-3 md:px-6 fixed top-0 left-0 right-0 h-14 md:h-16 lg:h-[4.5rem] w-screen overflow-hidden z-50 bg-darkBlue border-b">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <div className="max-w-28 md:max-w-32 lg:max-w-36  overflow-hidden mr-0 md:mr-3 lg:mr-4">
