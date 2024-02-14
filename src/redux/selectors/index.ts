7776dd142691cb4d60a1369ef67e9128f43a6410
Nijat Hamid <nicatorium@gmail.com>
2024-02-14 17:16:19 +0400

    Rtk removed,Web3Modal updated latest

--- src/redux/selectors/index.ts
+++
@@ -1,7 +0,0 @@
-import { RootState } from "@redux/store";
-import { createDraftSafeSelector } from "@reduxjs/toolkit";
-
-export const FormState = createDraftSafeSelector(
-    (state:RootState) => state.formModalState.open,
-    (open)=>open
-)
