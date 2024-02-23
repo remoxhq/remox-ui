6ac467b4bdf86299234a318a515b05cd67d64162
Nijat Hamid <nicatorium@gmail.com>
2024-02-23 15:45:24 +0400

    Dashboard Responsivness Done

--- src/pages/dashboard/components/orgInfo.tsx
+++ src/pages/dashboard/components/orgInfo.tsx
@@ -13,7 +13,7 @@ import { Link } from "react-router-dom";
 
 function OrgInfo() {
   const { toast } = useToast();
-  
+
   const copyToClipboard = async () => {
     try {
       await navigator.clipboard.writeText(window.location.href);
@@ -32,13 +32,13 @@ function OrgInfo() {
     }
   };
   return (
-    <section className="flex items-center gap-2">
-      <Avatar className="w-[72px] h-[72px]">
+    <section className="flex items-center flex-col md:flex-row gap-2">
+      <Avatar className="size-[60px] md:size-16 lg:size-[72px]">
         <AvatarImage src="/img/orglogo.png" alt="Organization Logo" className="object-cover" />
         <AvatarFallback className="bg-avatarbg border-2"></AvatarFallback>
       </Avatar>
       <div>
-        <div className="flex items-center gap-1">
+        <div className="flex items-center justify-center md:justify-normal gap-1">
           <h1 className="font-medium text-xl text-whitish">Lido</h1>
           <TooltipProvider delayDuration={100}>
             <Tooltip>
@@ -53,27 +53,33 @@ function OrgInfo() {
             </Tooltip>
           </TooltipProvider>
         </div>
-        <div className="flex items-center gap-5">
-          <p className="font-semibold text-2xl text-whitish">
-            <NR value={62210589981.77} short={false} />
-          </p>
-          <ND newValue={233} oldValue={23343} className="font-medium text-sm " />
+        <div className="flex items-center gap-2 md:gap-5  md:flex-row flex-col">
+          <div className="flex items-center gap-2 md:gap-5">
+            <p className="font-semibold text-lg sm:text-xl md:text-2xl text-whitish">
+              <NR value={62210589981.77} short={false} />
+            </p>
+            <ND newValue={233} oldValue={23343} className="font-medium text-sm " />
+          </div>
           <div className="py-1 px-5 border-l border-r flex items-center gap-3">
             <Link to="https://remox.io" target="_blank" className="group">
-              <Web className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in"/>
+              <Web className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in" />
             </Link>
             <Link to="https://remox.io" target="_blank" className="group">
-              <Twitter className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in"/>
+              <Twitter className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in" />
             </Link>
             <Link to="https://remox.io" target="_blank" className="group">
-              <Github className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in"/>
+              <Github className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in" />
             </Link>
             <Link to="https://remox.io" target="_blank" className="group">
-              <Discord className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in"/>
+              <Discord className="w-4 h-4 object-cover  lg:group-hover:scale-110 transition-all duration-200 ease-in" />
             </Link>
           </div>
-          <Button type="button" className="py-1 px-2 w-auto h-auto border border-brand bg-transparent rounded-[14px] font-semibold text-xs text-brand hover:bg-brand/10" onClick={copyToClipboard}>
-            <Share2 className="w-3 h-3 object-cover mr-1 "/> Share
+          <Button
+            type="button"
+            className="py-1 px-2 w-auto h-auto border border-brand bg-transparent rounded-[14px] font-semibold text-xs text-brand hover:bg-brand/10"
+            onClick={copyToClipboard}
+          >
+            <Share2 className="w-3 h-3 object-cover mr-1 " /> Share
           </Button>
         </div>
       </div>
