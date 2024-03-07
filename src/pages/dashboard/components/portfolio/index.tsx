e70e0cd72cdf847facc78c5729674f0cb2842df9
Nijat Hamid <nicatorium@gmail.com>
2024-03-07 19:18:01 +0400

    Archive scrolling issues fixed

--- src/pages/dashboard/components/portfolio/index.tsx
+++ src/pages/dashboard/components/portfolio/index.tsx
@@ -233,7 +233,7 @@ function Portfolio() {
         <TabsContent value="archive" className="overflow-auto h-full scrollbar-none w-full">
           {archiveData && !isArchiveDataPending && isArchiveDataSuccess && archiveData.result.name ?
             <div className="mb-8 min-w-[630px]">
-              <div className="flex items-center border-b *:pb-2 *:overflow-hidden w-full sticky top-2 mb-2 left-0 bg-darkBlue z-10">
+              <div className="flex items-center border-b *:pb-2 *:overflow-hidden w-full sticky top-0 pt-2 left-0 bg-darkBlue z-10">
                 <div className="w-[214px] pr-3 border-r ">
                   <div className="flex items-center gap-2 h-[26px] mb-4">
                     <p className="font-semibold text-xs text-whitish">HOLDINGS ON</p>
