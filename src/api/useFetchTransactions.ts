e3056d6732417247f56715e46173c19f40198dd4
Nijat Hamid <nicatorium@gmail.com>
2024-04-07 08:32:49 +0400

    Transaction sorting solved. Pagination changed from İnfinite Scroll to Load More

--- src/api/useFetchTransactions.ts
+++ src/api/useFetchTransactions.ts
@@ -1,43 +1,55 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
+import dayjs from "dayjs";
 
+type TxsResponse = {
+  id: string;
+  hash: string;
+  assetName: string;
+  assetLogo: string;
+  from: string;
+  to: string;
+  direction: string;
+  count: number;
+  amount: number;
+  date: string;
+  chain: string;
+};
 type Response = {
   statusCode: number;
   success: boolean;
   result: {
-    txs: {
-      id:string
-      hash: string;
-      assetName: string;
-      assetLogo: string;
-      from: string;
-      to: string;
-      direction: string;
-      count: number;
-      amount: number;
-      date: string;
-      chain: string;
-    }[];
-    next:string | undefined
+    txs: TxsResponse[];
+    next: string | undefined;
   };
 };
 
 type IProps = string | undefined;
 
-const fetch = async (dashboardLink: IProps,pageParam:string): Promise<Response | undefined> => {
+const fetch = async (dashboardLink: IProps, pageParam: string): Promise<Response | undefined> => {
   if (!dashboardLink) {
     return undefined;
   }
-  const transformedParam = pageParam.length >10 ? pageParam : JSON.stringify(pageParam)
+  const transformedParam = pageParam.length > 10 ? pageParam : JSON.stringify(pageParam);
   const response = await axios.get<Response>(`${import.meta.env.VITE_Base_API}/treasury/txs/${dashboardLink}/${transformedParam}`).then((res) => res.data);
   return response;
 };
 export const useFetchTransactions = (dashboardLink: IProps) =>
   useInfiniteQuery({
     queryKey: ["useFetchTransactions", dashboardLink],
-    queryFn: ({pageParam}) => fetch(dashboardLink,pageParam),
+    queryFn: ({ pageParam }) => fetch(dashboardLink, pageParam),
     staleTime: 10 * (60 * 1000), //10 mins
     enabled: !!dashboardLink,
     initialPageParam: "",
-    getNextPageParam: (lastPage) => lastPage?.result.next ? lastPage?.result.next : undefined,
+    getNextPageParam: (lastPage) => (lastPage?.result.next ? lastPage?.result.next : undefined),
+    select: (data) => {
+      const flattenedTxs: TxsResponse[] = [];
+      for (const page of data.pages) {
+        if (page?.result?.txs) {
+          flattenedTxs.push(...page.result.txs);
+        }
+      }
+      const result = flattenedTxs.sort((a, b) => dayjs(b.date).unix() - dayjs(a.date).unix());
+      return result;
+    },
   });
