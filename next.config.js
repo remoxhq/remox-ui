3816a0bb46694c4bb0c993fcc31a74fdb43b41d3
ilhambagirov <81701005+ilhambagirov@users.noreply.github.com>
2024-05-21 21:02:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,13 @@ const BLOG_URL  = "https://remox.io";
 const nextConfig = {
   async rewrites() {
     return [
-      {
-        source: "/:path*",
-        destination: `/:path*`,
+        {
+        source: "risk/aave",
+        destination: `https://risk.remox.io/aave`,
       },
       {
-        source: "/aave",
-        destination: `https://risk.remox.io/aave`,
+        source: "/:/*",
+        destination: `/:/*`,
       }
     ];
   },
